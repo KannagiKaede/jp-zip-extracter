8203f46dfc10c10c0d6bbe43c0f0f136dd716b0d
Kaede <290378514+KannagiKaede@users.noreply.github.com>
2026-08-20 08:03:54 +0800

    init

---
+++ jp-zip-extracter/jp-zip-extracter/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace jp_zip_extracter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
