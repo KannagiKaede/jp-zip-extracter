87732223da4c6bd5a4027f166b8db94cb813d751
Kaede <290378514+KannagiKaede@users.noreply.github.com>
2026-09-01 18:39:29 +0800

    Select file button with basic textbox ui

--- jp-zip-extracter/jp-zip-extracter/MainWindow.xaml.cs
+++ jp-zip-extracter/jp-zip-extracter/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +16,40 @@ namespace jp_zip_extracter
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+        private string filePath;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string FilePath
+        {
+            get { return filePath; }
+            set 
+            { 
+                filePath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FilePath"));
+            }
+        }
+
+
+        private void selectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            bool? isSelectedFile = fileDialog.ShowDialog();
+
+            if (isSelectedFile == true) 
+            {
+                string path = fileDialog.FileName;
+                FilePath = path;
+            }
+
+            
         }
     }
 }
